57838b7efed66413ed3aa467b76e06dc900f45e3
SamaaKH99 <93490251+SamaaKH99@users.noreply.github.com>
2022-04-16 16:33:24 +0200

    secondCommit

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-
+  //Takes input from the recipe model and recipe array
+  @Input() recipe!: Recipe;
+  // Output so we can listen to this event from outside the component
+  @Output() recipeSelected = new EventEmitter<void>();
   constructor() { }
 
-  ngOnInit(): void {
+  @Output() recipeSelectedDetails = new EventEmitter<void>();
+  toDetails(){
+    //window.scrollTo(0,document.body.scrollHeight);
+    window.scrollTo(0, document.body.scrollHeight || document.documentElement.scrollHeight);
+    //this.recipeSelectedDetails.emit();
+    //document.getElementById('details')?.scrollIntoView();
+  }
+
+
+
+  onSelected(){
+    this.recipeSelected.emit();
+  }
+
+  ngOnInit(){
   }
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,12 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+  @Output() recipeWasSelectedDetails = new EventEmitter<string>();
+  //isShown: boolean = true ; // hidden by default
+
   // array of recipes of type Recipe model (array of Recipe model props)
   recipes: Recipe[] = [
     new Recipe('Mexican Chicken Burger', 
@@ -28,7 +34,22 @@ export class RecipeListComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
+
+
+  onRecipeSelectedDetails(details: string){
+    this.recipeWasSelectedDetails.emit(details);
+  }
+
+
+  // toggleShow() {
+
+  //   this.isShown = ! this.isShown;
+  // }
 
 }
